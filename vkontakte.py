1f0be71a69e7bd659258dcf14e8dc4ababd2287a
Alexander Konovalov <zanderghost232@gmail.com>
2017-09-09 12:52:14 +0900

    no message

--- vkontakte.py
+++ vkontakte.py
@@ -7,8 +7,8 @@ VERSION = '5.65'
 VK_API = 'https://api.vk.com/method/'
 
 user_name = 'tim_leary'
-user_id = 5030613
-token = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+user_id = None
+token = None
 
 params = {
     'access_token': token,
